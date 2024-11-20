5cc38c7a6a77eb15a52175f5ec2cc8c207ce5a49
Mohomed Afran <frmafran2004@gmail.com>
2024-11-21 05:24:19 +0530

    adding to origin

---
+++ first_chat_app/src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from "react-chat-engine";
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="2cefabdd-0744-43e7-aab7-9725cea8e6f5"
+            username="afran"
+            userSecret="afran2004"
+        />
+    )
+}
+
+export default App;
---
+++ youtube_clone/src/components/ChannelDetail.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
+
+const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  console.log(channelDetail, videos)
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+    .then(data => setChannelDetail(data?.items[0]));
+    
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+    .then(data => setVideos(data?.items));
+  }, [id]);
+
+    return (
+      <div>
+        {id}
+      </div>
+    )
+}
+
+export default ChannelDetail
